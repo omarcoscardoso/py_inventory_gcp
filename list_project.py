40f702d6178300058906657a2d3145fd80103e96
Marcos Cardoso de Oliveira <cardoso.oliveira@gmail.com>
2024-04-15 15:08:23 -0300

    Add contagem de linhas

--- list_project.py
+++ list_project.py
@@ -15,7 +15,7 @@ from googleapiclient import discovery
 
 # Inicializar variáveis
 dir_path = os.path.dirname(os.path.realpath(__file__)) # Busca o dir real
-# filename = dir_path+'/csv/'+'lista_GCP_projetos.csv'
+count = 1 
 
 # Carregar credenciais do arquivo de serviço
 service_account_info = dir_path+'/credentials/'+'<NOME DO SEU ARQUIVO JSON COM A KEY>'
@@ -25,12 +25,14 @@ credentials = service_account.Credentials.from_service_account_file(service_acco
 service = discovery.build('cloudresourcemanager', 'v1', credentials=credentials)
 request = service.projects().list()
 
-print('{:<40} {:<30} {:<20}'.format('PROJECT_ID', 'NAME', 'PROJECT_NUMBER'))
+print('{:>3} {:<40} {:<30} {:<20}'.format('', 'PROJECT_ID', 'NAME', 'PROJECT_NUMBER'))
+
 
 while request is not None:
     response = request.execute()
 
     for project in response.get('projects', []):
-        print('{:<40} {:<30} {:<20}'.format(project['projectId'], project['name'], project['projectNumber']))
-
+        print('{:>3} {:<40} {:<30} {:<20}'.format(count, project['projectId'], project['name'], project['projectNumber']))
+        count += 1
+    
     request = service.projects().list_next(previous_request=request, previous_response=response)
